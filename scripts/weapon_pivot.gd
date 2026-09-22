59dcffb2b808f9979e0fbfd8dd9597fc77f31e85
Lucas Cavallin Caczmareki <lucascaczmareki@gmail.com>
2026-09-21 23:43:07 -0300

    feat: initial structure for weapon

---
+++ scripts/weapon_pivot.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# ill need to understand the logic and method to rotate a node
+# next steps is to study this and then try to program
+
+# @onready var weapon_sprite: Sprite2D = $Weapon
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# weapon_sprite.show()
+	pass
